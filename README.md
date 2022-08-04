46a32a3214861c14ce6b3c1f4e748b5dc28d7162
Julian McMillan <julianmcm76@gmail.com>
2022-08-04 13:31:05 +0200

    Wrote functionaly for custom sketch field, edited README.md

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Etch-a-Sketch
+><br>
+>
+><h2>Description</h2>
+>
+>This project is an assignment for beginner programmers and is meant to help them get comfortable with JavaScript programming, DOM manipulation and loops.
+>
+>The project is a simple one-page JavaScript application which creates a sketch field and "draws" on the field by coloring in divs which make it up when the mouse hovers over it.
+>
+><br>
+>
+><h2>Aim</h2>
+>Design a simple browser application which creates a fixed-size container wherein different grid-sizes can be applied and wherein the grid-points (made up by divs) are filled with color when the mouse cursor hovers over them.
+>
+><br>
+>
+><a src= "#" target = _blank>Access a live version of Etch-a-Sketch here</a>
+>
+><br>
+>
+>
+>
+>
+>
+><br>
+>
+><h2>Instructions on how to play</h2>
+>
+>- Once the page has loaded, the page will display four buttons to the left and a sketh-field to the right
+>- A default sketch size is already applied and will start coloring divs inside the sketch field with the default color of black when the mouse cursor hovers over it
+>- You can change the sketch size by clicking on one of the buttons to the right with options: small, medium & large. Clicking one of these buttons will also cause the color of the divs inside the sketch field to change to a new randomly selected color each time the mouse cursor is hovered over it.
+>- To return to the default sketch-field, click on the 'normal' button to the left which will reload the default state, or simply refresh the tab the page is open in.
+>
+><br>
+>
+><br>
+>
+><h2>If you wish to copy this project and work on your own version, follow the instructions below</h2>
+>
+>- Ensure that you have GIT installed and a linked Github account, open your CLI.
+>- Create a new directory on your local machine and call it 'Etch-a-Sketch-js'
+>- Return to the repository on Github and click the green 'code' button, and select between 'SSH', 'HTTPS' or 'Github CLI' depending on your configuration and preferences.
+>- In the CLI navigate to the newly created 'Etch-a-Sketch-js' directory.
+>- Clone this project's repository into this directory with command: <code>git clone  'link that you copied'</code>
+>- Ensure you have a valid code editor installed and open the directory in your code editor.
+>
+><br>
+
+
+
+
+
--- script.js
+++ script.js
@@ -5,43 +5,114 @@
 //for each row set height to content-container height / input number
 //for each div add class .row-divs
 //for each div set width to row-width / input number
-//define a function which generates a random background-color for each div when div is hovered over and
-//increases 10% darkness until it is completely black
+//define a function which generates a random background-color for div when div is hovered over
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const contentContainer = document.querySelector('.content-container');
 
-function mouseOverHandler(e) {//changes background color when mouseover event is triggered on row divs
+const contentContainer = document.querySelector('.content-container'); //Get UI DOM
+const sizeBtns = document.querySelectorAll('.btns');
+
+
+////////////////////////////////RUNS ON PAGE LOAD (DEFAULT GRID)////////////////////////////////////////////
+
+
+function mouseOverHandler(e) {                      //changes background color when mouseover
+                                                    //event is triggered on row divs
     return e.target.style.backgroundColor = 'black';
- }
+}
 
-for(let i = 0; i <= 16; i++){//loops to create x amount of rows 
+for (let i = 0; i <= 8; i++) {                      //loops to create x amount of rows 
     let row = document.createElement('div');
 
     row.classList.add('grid-rows');
     row.setAttribute('id', `row-${i}`);
-    row.style.height = `${contentContainer.offsetHeight / 16}px`;//row height is set to = container height / by x amount of desired rows
+    row.style.height =
+        `${contentContainer.offsetHeight / 8}px`;  //row height is set to = 
+                                                   //container height / by x amount of desired rows
 
-    contentContainer.appendChild(row);//after creating row, insert it inside content container
+    contentContainer.appendChild(row);             //after creating row, insert it inside content container
 
-    for(let j = 0; j <= 16; j++){// loop runs on each iteration of i (row creation) to add divs (color points)
-        let div = document.createElement('div');//create a new div element
+    for (let j = 0; j <= 16; j++) {                //loop runs on each iteration of i (row creation) 
+                                                   //to add divs (color points)
+        let div = document.createElement('div');   //create a new div element
 
         div.classList.add('row-divs');
         div.setAttribute('id', `row-${i}_div-${j}`);
+        div.style.width = `${row.clientWidth / 16}px`;
 
-        div.style.width = `${ row.clientWidth / 16}px`;
-        contentContainer.addEventListener('mousedown', (e) => {//will only start coloring if left mouse is clicked and held in
-            div.addEventListener('mouseover',mouseOverHandler)
-        })
+        div.addEventListener('mouseover', mouseOverHandler);
 
         row.appendChild(div);
     }
-    //.appendChild(row)
-    
+}
+///////////////////////////////////////////END OF DEFAULT GRID/////////////////////////////////////////////////
+
+
+function runNewGrid(num) {                                  //define function to create new grid
+                                                            //when option buttons are clicked
+    for (let i = 0; i <= (num / 2); i++) { 
+                                                            //loops to create x amount of rows 
+        let row = document.createElement('div');
+        row.classList.add('grid-rows');
+        row.setAttribute('id', `row-${i}`);
+        row.style.height =
+           `${contentContainer.offsetHeight / (num / 2)}px`;//row height is set to = container height / by 
+         
+                                                            //x amount of desired rows
+        contentContainer.appendChild(row);                  //after creating row, insert it inside content container
+
+        for (let j = 0; j <= num; j++) {                    //loop runs on each iteration of i (row creation) to add divs (color points)
+
+            let div = document.createElement('div');        //create a new div element
+
+            div.classList.add('row-divs');
+            div.setAttribute('id', `row-${i}_div-${j}`);
+            div.style.width = `${row.clientWidth / num}px`;
+
+            div.addEventListener('mouseover', (e) => {
+                mouseOverHandler(e);
+                randomBackgroundColor(e);                   //generate random background-color on hover
+            })
+
+            row.appendChild(div);
+        }
+    }
 }
 
-document.querySelectorAll('.row-divs').forEach(element => {
-element.addEventListener('mouseup', (e) => document.querySelectorAll('.row-divs').forEach(element =>
-    element.removeEventListener('mouseover', mouseOverHandler)))
-});//attach an event listener for when mouse button is released so that mouseover handler
-// function is removed from all divs preventing further divs from being colored
+
+function randomBackgroundColor(e) {                         //define function to generate random background color
+
+    const randomColorVal =                                 //define a function to select a random num between 2 numbers
+        (min, max) => min + Math.floor
+            (Math.random() * (max - min + 1));
+
+    let red = randomColorVal(0, 255);                  //assign a value to RGB variables
+    let green = randomColorVal(0, 255);
+    let blue = randomColorVal(0, 255);
+
+    e.target.style.backgroundColor =                   //set current div background color to RGB variables 
+        `rgb(${red}, ${green}, ${blue})`;
+}
+
+function changeSize(num) {                                 //define function to run when
+    //grid size option buttons are clicked
+
+    let gridSize = num;
+
+    while (contentContainer.firstChild) {                  //removes default grid or previous grid
+        contentContainer.removeChild(contentContainer.firstChild);
+    }
+
+    runNewGrid(gridSize);
+}
+
+sizeBtns.forEach(element =>
+
+    element.addEventListener('click', (e) => {
+        changeSize(Number(e.target.id));
+    }));
+
+
+function reset() {
+    document.location.reload()
+}
